adf2ec64eebb2c26b8029a3417f3b51bcbe7af5d
Nikita Vakula <nikita.vakula.dev@gmail.com>
2025-08-01 16:09:45 +0200

    Add CLI
    Signed-off-by: Nikita Vakula <nikita.vakula.dev@gmail.com>

---
+++ cmd/configureBridge.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/krjakbrjak/network-utils/src/utils/network/firewall"
+	"github.com/spf13/cobra"
+)
+
+// configureBridgeCmd represents the configure-bridge command
+var configureBridgeCmd = &cobra.Command{
+	Use:   "configure-bridge",
+	Short: "configures a network bridge",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name, nameErr := cmd.Flags().GetString("name")
+		if nameErr != nil {
+			return nameErr
+		}
+		hostIf, hostIfErr := cmd.Flags().GetString("hostIf")
+		if hostIfErr != nil {
+			return hostIfErr
+		}
+		nftPrefix, nftPrefixErr := cmd.Flags().GetString("nftPrefix")
+		if nftPrefixErr != nil {
+			return nftPrefixErr
+		}
+
+		if jumpErr := firewall.AddJumpRule("FORWARD", fmt.Sprintf("%sFORWARD", nftPrefix), "filter"); jumpErr != nil {
+			return jumpErr
+		}
+
+		if jumpErr := firewall.AddJumpRule("INPUT", fmt.Sprintf("%sINPUT", nftPrefix), "filter"); jumpErr != nil {
+			return jumpErr
+		}
+
+		rules, rulesErr := firewall.NewRules(
+			firewall.ForwardOutboundRule(fmt.Sprintf("%sFORWARD", nftPrefix), "filter", hostIf, name),
+			firewall.ForwardReturnTrafficRule(fmt.Sprintf("%sFORWARD", nftPrefix), "filter", hostIf, name),
+			firewall.MasqueradeRule("POSTROUTING", "nat", hostIf),
+			firewall.PortRule(53, "udp", fmt.Sprintf("%sINPUT", nftPrefix), "filter"),
+			firewall.PortRule(67, "udp", fmt.Sprintf("%sINPUT", nftPrefix), "filter"),
+			firewall.PortRule(68, "udp", fmt.Sprintf("%sINPUT", nftPrefix), "filter"),
+			firewall.PortRule(53, "tcp", fmt.Sprintf("%sINPUT", nftPrefix), "filter"),
+			firewall.PortRule(67, "tcp", fmt.Sprintf("%sINPUT", nftPrefix), "filter"),
+			firewall.PortRule(68, "tcp", fmt.Sprintf("%sINPUT", nftPrefix), "filter"),
+		)
+
+		if rulesErr != nil {
+			return rulesErr
+		}
+
+		return firewall.AddRules(rules)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configureBridgeCmd)
+
+	configureBridgeCmd.Flags().StringP("name", "n", "", "Name of the bridge to configure")
+	configureBridgeCmd.MarkFlagRequired("name")
+	configureBridgeCmd.Flags().String("hostIf", "", "Host interface that the bridge will use")
+	configureBridgeCmd.MarkFlagRequired("hostIf")
+	configureBridgeCmd.Flags().String("nftPrefix", "QEMU-", "Prefix for nftables rules")
+}
---
+++ cmd/createTap.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/krjakbrjak/network-utils/src/utils/network/ifc"
+	"github.com/spf13/cobra"
+)
+
+// createTapCmd represents the create-tap command
+var createTapCmd = &cobra.Command{
+	Use:   "create-tap",
+	Short: "Creates a tap device",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name, nameErr := cmd.Flags().GetString("name")
+		if nameErr != nil {
+			return nameErr
+		}
+		bridgeName, bridgeErr := cmd.Flags().GetString("bridge")
+		if bridgeErr != nil {
+			return bridgeErr
+		}
+
+		return ifc.CreateTap(name, bridgeName)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createTapCmd)
+
+	createTapCmd.Flags().StringP("name", "n", "", "Name of the tap device to create")
+	createTapCmd.MarkFlagRequired("name")
+	createTapCmd.Flags().String("bridge", "", "Name of the bridge to attach the tap device to")
+	createTapCmd.MarkFlagRequired("bridge")
+}
