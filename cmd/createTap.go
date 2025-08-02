f4d0e2f1ea56ec132c8c4266d78ceb4c83af0bd4
Nikita Vakula <nikita.vakula.dev@gmail.com>
2025-08-02 11:52:28 +0200

    Cleanup
    Signed-off-by: Nikita Vakula <nikita.vakula.dev@gmail.com>

--- cmd/createTap.go
+++ cmd/createTap.go
@@ -5,7 +5,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// createTapCmd represents the create-tap command
 var createTapCmd = &cobra.Command{
 	Use:   "create-tap",
 	Short: "Creates a tap device",
