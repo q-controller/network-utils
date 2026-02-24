78f80b9108f8a561f9b4798055c9fdddcedd5ef1
Nikita Vakula <programmistov.programmist@gmail.com>
2026-02-24 10:07:09 +0100

    Allow inbound forwarding for cross-host VM traffic
    The existing ForwardReturnTrafficRule restricted inbound
    traffic to RELATED|ESTABLISHED connections, which blocked
    NEW connections from peer hosts destined for VMs in the
    namespace.
    
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- src/utils/network/firewall/forward.go
+++ src/utils/network/firewall/forward.go
@@ -62,22 +62,6 @@ func ForwardReturnTrafficRule(chainName, tableName, hostIf, internalIf string) N
 				&expr.Meta{Key: expr.MetaKeyOIFNAME, Register: 2},
 				// [ cmp eq reg 2 interfaceA ]
 				&expr.Cmp{Op: expr.CmpOpEq, Register: 2, Data: []byte(internalIf + "\x00")},
-				// [ ct lookup state => reg 1 ]
-				&expr.Ct{Register: 1, Key: expr.CtKeySTATE, SourceRegister: false},
-				// [ bitwise and reg 1 with RELATED|ESTABLISHED ]
-				&expr.Bitwise{
-					DestRegister:   1,
-					SourceRegister: 1,
-					Len:            4,
-					Mask:           []byte{0x06, 0x00, 0x00, 0x00}, // RELATED(0x02) | ESTABLISHED(0x04)
-					Xor:            []byte{0x00, 0x00, 0x00, 0x00},
-				},
-				// [ cmp ne reg 1 0 ]
-				&expr.Cmp{
-					Op:       expr.CmpOpNeq,
-					Register: 1,
-					Data:     []byte{0x00, 0x00, 0x00, 0x00},
-				},
 				// [ immediate verdict ACCEPT ]
 				&expr.Verdict{Kind: expr.VerdictAccept},
 			},
